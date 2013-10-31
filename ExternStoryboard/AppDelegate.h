d3cc3cf869a626a7e3e71bb0d3c15e031738a1c4
Paul Malikov <vokilam@gmail.com>
2013-10-31 11:45:51 +0300

    Import demo project

---
+++ ExternStoryboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test-segue-multiple-storiboards
+//
+//  Created by Paul Malikov on 10/31/13.
+//  Copyright (c) 2013 Paul Malikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ExternStoryboard
 ================
 
 This demo demonstrates how to link view controllers with segues between multiple storyboards
+
+Inspired by [RBStoryboardLink][1].
+
+[1]:https://github.com/rob-brown/RBStoryboardLink
